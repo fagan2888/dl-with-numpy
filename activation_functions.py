20dbe7bb828d93397648cbca402ee6a693464b79
Rohit Midha <rohit.midha23@gmail.com>
2019-03-28 11:38:28 +0530

    added : tanh

--- activation_functions.py
+++ activation_functions.py
@@ -12,3 +12,9 @@ class sigmoid():
         return 1 / (1 + np.exp(-x))
     def gradient(self,x):
         return self.__call__(x) * (1 - self.__call__(x))
+
+class tanh():
+    def __call__(self,x):
+        return (np.exp(2x) - 1) * (np.exp(2x) + 1)
+    def gradient(self,x):
+        return 1 - np.pow(self.__call__(x),2)
