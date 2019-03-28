d4e79fba6f2e4fb74bd20556302a739457419941
Rohit Midha <rohit.midha23@gmail.com>
2019-03-28 11:51:07 +0530

    added : softmax

--- activation_functions.py
+++ activation_functions.py
@@ -49,3 +49,12 @@ class SELU():
 
     def gradient(self, x):
         return self.scale * np.where(x >= 0.0, 1, self.alpha * np.exp(x))
+
+class Softmax():
+    def __call__(self, x):
+        e_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
+        return e_x / np.sum(e_x, axis=-1, keepdims=True)
+
+    def gradient(self, x):
+        p = self.__call__(x)
+        return p * (1-p)
