ffdb767382e30dc27bc81228c73e049c4e123686
Rohit Midha <rohit.midha23@gmail.com>
2019-03-28 11:35:05 +0530

    added : sigmoid

--- activation_functions.py
+++ activation_functions.py
@@ -1,6 +1,14 @@
 # Activation Functions
+import numpy as np
+
 class identity():
     def __call__(self,x):
         return x
     def gradient(self,x):
         return 1
+
+class sigmoid():
+    def __call__(self,x):
+        return 1 / (1 + np.exp(-x))
+    def gradient(self,x):
+        return self.__call__(x) * (1 - self.__call__(x))
