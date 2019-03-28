0c4762e5836c2b1a49749ce2d3e85e732339fcdb
Rohit Midha <rohit.midha23@gmail.com>
2019-03-28 11:44:04 +0530

    added : leaky relu

--- activation_functions.py
+++ activation_functions.py
@@ -1,26 +1,32 @@
 # Activation Functions
 import numpy as np
 
-class identity():
+class Identity():
     def __call__(self,x):
         return x
     def gradient(self,x):
         return 1
 
-class sigmoid():
+class Sigmoid():
     def __call__(self,x):
         return 1 / (1 + np.exp(-x))
     def gradient(self,x):
         return self.__call__(x) * (1 - self.__call__(x))
 
-class tanh():
+class TanH():
     def __call__(self,x):
         return (np.exp(2x) - 1) * (np.exp(2x) + 1)
     def gradient(self,x):
         return 1 - np.pow(self.__call__(x),2)
 
-class relu():
+class ReLu():
     def __call__(self,x):
         return np.where(x>=0,x,0)
     def gradient(self,x):
         return np.where(x>=0,1,0)
+
+class LeakyReLu():
+    def __call__(self,x):
+        return np.where(x>=0,x,0.01 * x)
+    def gradient(self,x):
+        return np.where(x>=0,1,0.01)
