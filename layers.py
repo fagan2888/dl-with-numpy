7b3c98358a81d516839fc4986c8eb7414d09015b
Rohit Midha <rohit.midha23@gmail.com>
2019-05-20 01:50:47 +0530

    added : dense layer

--- layers.py
+++ layers.py
@@ -12,7 +12,6 @@ class Layer(object):
     def layer_name(self):
         return self.__class__.__name__
     def parameters(self):
-
         return 0
 
     def forward_pass(self, X, training):
@@ -23,3 +22,19 @@ class Layer(object):
 
     def output_shape(self):
         raise NotImplementedError()
+class DenseLayer(Layer):
+    def __init__(self, n_units, input_shape=None):
+        self.layer_input = None
+        self.input_shape = input_shape
+        self.n_units = n_units
+        self.trainable = True
+        self.W = None
+        self.w0 = None
+
+    def initialize(self, optimizer):
+        limit = 1 / math.sqrt(self.input_shape[0])
+        self.W  = np.random.uniform(-limit, limit, (self.input_shape[0], self.n_units))
+        self.w0 = np.zeros((1, self.n_units))
+        # Weight optimizers
+        self.W_opt  = copy.copy(optimizer)
+        self.w0_opt = copy.copy(optimizer)
